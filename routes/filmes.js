7a09f444d38b6ecc95ddb8833652fb7501716e65
IgorGSarmento <igor_gss@hotmail.com>
2018-10-03 16:57:20 -0300

    Configura as rotas de consulta para os filmes.

--- routes/filmes.js
+++ routes/filmes.js
@@ -1,26 +1,62 @@
 var express = require('express');
+
+var mongoose = require('mongoose');
+
+var Filme = require('../models/filmes')
+
 var router = express.Router();
+
+function invert_date (oldDate){
+  var data = oldDate.split("-");
+  let newDate = data[2]+'-'+data[1]+'-'+data[0];
+
+  return newDate;
+}
 
 /* GET | lista de filmes. */
 router.get('/consultar', function(req, res, next) {
-  var db = req.db;
-  var collection = db.get('filmes');
-  collection.find({},{},function(e,docs){
-    res.json(docs);
+  Filme.find({}, function(err, filmes){
+    if (err) {
+      return res.send(err)
+    }
+
+    res.json(filmes);
   });
 });
 
-/* GET | consulta um filme. */
-router.get('/consultar/:filme', function(req, res, next) {
-  res.send('Got a GET request');
-  console.log('Filme:', req.params.filme);
+/* GET | consulta um filme por titulo. */
+router.get('/consultar/:titulo', function(req, res, next) {
+  let titulo_filme = req.params.titulo;
+
+  Filme.find({titulo: new RegExp(titulo_filme, 'i')}, function(err, filmes){
+    if (err) {
+      return res.send(err)
+    }
+
+    res.json(filmes);
+  });
 });
 
 /* GET | consulta filmes entre data de estreia. */
-router.get('/consultar/:filmedti/:filmedtf', function(req, res, next) {
-  res.send('Got a GET request');
-  console.log('Filme data inicio:', req.params.filmedti);
-  console.log('Filme data fim:', req.params.filmedtf);
+router.get('/consultar/:dtainicio/:dtafim', function(req, res, next) {
+  let data_inicio = invert_date(req.params.dtainicio);
+  let data_fim = invert_date(req.params.dtafim);
+
+  console.log(data_inicio);
+  console.log(data_fim);
+
+  Filme.find({
+    dtaestreia : {
+      '$gte': new Date(data_inicio),
+      '$lte': new Date(data_fim)
+    }
+  }, function(err, filmes){
+    if (err) {
+      return res.send(err)
+    }
+
+    res.json(filmes);
+  });
 });
 
 /* POST | insere um filme por url. */
@@ -28,14 +64,14 @@ router.post('/inserir', function(req, res, next){
 	res.send('Got a POST request');
 });
 
-/* PUT | atualiza um filme a partir do nome. */
-router.put('/atualizar/:filme', function (req, res) {
+/* PUT | atualiza um filme a partir do titulo. */
+router.put('/atualizar/:titulo', function (req, res) {
   res.send('Got a PUT request');
   console.log('Atualiza filme:', req.params.filme);
 });
 
-/* DELETE | apaga um filme pelo nome. */
-router.delete('/apagar/:filme', function (req, res) {
+/* DELETE | apaga um filme pelo titulo. */
+router.delete('/apagar/:titulo', function (req, res) {
   res.send('Got a DELETE request');
   console.log('Delata filme:', req.params.filme);
 });
